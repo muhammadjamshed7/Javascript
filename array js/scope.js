113b1e3cdaf58104a0ee91d0a514dddfaacac222
muhammadjamshed <ajmehsud12@gmail.com>
2024-09-19 18:07:16 +0500

    scope level js learning

---
+++ array js/scope.js
@@ -0,0 +1,30 @@
+if (true) {
+  let a = 10;
+  const b = 20;
+  var c = 30;
+}
+
+// console.log(a)
+// console.log(b)
+console.log(c)
+function one(){
+    const username="jimi"
+    function two(){
+        const website="swl.tech"
+        console.log(username)
+    }
+    // console.log(website)
+    two()
+}
+one()
+
+if(true){
+    const username="jamsshed"
+    if(username==="jamsshed"){
+        const website=" jimi ki site"
+        console.log(username+website)
+    }
+    // console.log(website)
+
+}
+// console.log(username)
--- array js/shopingcart.js
+++ array js/shopingcart.js
@@ -1,8 +1,31 @@
 // function calculateCartPrice(...num1){
 //     return num1
 // }
-// console.log(calculateCartPrice(200,400,600))
-function calculateCartPrice(v1,v2,v3,...num1){
-    return num1
+// // console.log(calculateCartPrice(200,400,600))
+// function calculateCartPrice(v1,v2,v3,...num1){
+//     return num1
+// }
+// console.log(calculateCartPrice(200,400,600,500,800,101,222))
+// const user={
+//     username:'jamshed',
+//     prize:'199'
+// }
+// function handleObject(anyobject){
+//     console.log(`username is ${anyobject.username} and prize is ${anyobject.prize}`)
+
+// }
+// console.log(handleObject(user))
+
+const getArraydata=[1,2,3,4,5]
+function getArray(){
+    getArraydata.forEach((item,index)=>console.log(index,": ",item))
 }
-console.log(calculateCartPrice(200,400,600,500,800,101,222))
+// console.log(getArray())
+getArray()
+const objeFunction=(obj)=>{
+    return `My name is ${obj.name} and ${obj.age} years old`
+}
+console.log(objeFunction({
+    name:'jamshed',
+    age:23
+}))
