c37b262b1a1302c87af54b178aaf6a8e1df4ad6e
muhammadjamshed <ajmehsud12@gmail.com>
2024-09-20 12:28:26 +0500

    arrow function and this keyword

--- array js/scope.js
+++ array js/scope.js
@@ -6,12 +6,12 @@ if (true) {
 
 // console.log(a)
 // console.log(b)
-console.log(c)
+// console.log(c)
 function one(){
     const username="jimi"
     function two(){
         const website="swl.tech"
-        console.log(username)
+        // console.log(username)
     }
     // console.log(website)
     two()
@@ -21,10 +21,39 @@ one()
 if(true){
     const username="jamsshed"
     if(username==="jamsshed"){
-        const website=" jimi ki site"
-        console.log(username+website)
+        // const website=" jimi ki site"
+        // console.log(username+website)
     }
     // console.log(website)
 
 }
 // console.log(username)
+
+
+
+const user={
+    username:"jamshed",
+    price:999,
+    welcomemessage:function(){
+        console.log(`${this.username}, welcome to website`)
+        console.log(this)
+    }
+}
+// user.welcomemessage()
+// user.username="hitesh"
+// user.welcomemessage()
+
+// function chai(){
+//     const username={
+//         username:'jamshed',
+//     }
+//     console.log(this.username)
+// }
+// chai()
+
+// const addtwo=(num1,num2)=>num1+num2
+const addtwo=(num1,num2)=>({
+    username:'hitesh'
+
+})
+console.log(addtwo(3,4))
